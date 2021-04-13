bfeb03d2a107a2ab455140cb919f0f0dd1fa00ae
karaziyahya <karazi.yahya@gmail.com>
2021-04-13 11:10:34 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,91 @@
+/* 
+var maVar ="ma valeur";
+(function(){
+    var maVar = "Valeur locale";
+    console.log(maVar);
+})();
+*/
+
+/*
+let numero1 =5;
+let numero2 =4;
+let resultat;
+
+function Calcul(num1,num2){
+    resultat =num1*num2 ;
+    return resultat
+};
+
+console.log(Calcul(numero1,numero2));
+*/
+
+
+
+
+
+
+
+/*
+
+function carre(b) { 
+
+    return b*b; 
+
+} 
+
+// Afficher la sortie 
+
+console.log(carre(2)); 
+
+console.log(carre(4)); 
+
+
+
+function test(a,b) { 
+
+    return a===b;
+
+
+} 
+*/
+
+/*
+function reverseTab(tab) { 
+
+    let res = []; 
+  
+    for (let i = tab.length -1; i > -1; i--) { 
+  
+      res.push(tab[i]); 
+  
+    } 
+  
+  
+    return res; 
+  
+  } 
+  
+  // Afficher la sortie 
+  
+  console.log(reverseTab([1, 2, 3])); 
+  
+  console.log(reverseTab([1, 1, 2, 2, 3])); 
+  */
+
+
+  function reverseTab(tab) { 
+
+    let res = ["A","B","C"]; 
+  
+    for (let i = tab.length -1; i > -1; i--) { 
+  
+      res.push(tab[i]); 
+  
+    } 
+  
+  
+    return res; 
+  
+  } 
+
+
